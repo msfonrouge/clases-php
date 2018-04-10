aea91406726b8a6df262407ea171f5de6368fe6f
Segun <segun@MacBook-Pro-de-Segun.local>
2018-04-10 11:46:17 -0300

    clase 2

--- clase-2/index.php
+++ clase-2/index.php
@@ -64,3 +64,59 @@ if ($nombreDeUsuario == "admin" && $contraseniaDeUsuario == "1234") {
 echo "<br>";
 echo "<br>";
 echo "<br>";
+
+$edad = 20;
+$casado = true;
+$sexo = "Otro";
+
+if ($edad > 18 && !$casado) {
+  echo "Bienvenido";
+} elseif ($sexo == "Otro") {
+  echo "Bienvenido";
+}
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$cantidadDeAlumnos = 100;
+if ($cantidadDeAlumnos) {
+  echo "true";
+} else {
+  echo "false";
+}
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+if ($i = 0) {
+  echo "true";
+} else {
+  echo "false";
+}
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$numero = 20;
+$esParoNo = $numero % 2 == 0 ? "El número es par" : "El número es impar";
+echo $esParoNo;
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$nombre = "Pedro";
+switch ($nombre) {
+  case 'Pedro':
+    echo "Hola $nombre";
+    break;
+  case 'Julio':
+    echo "Hola $nombre";
+    break;
+  default:
+    echo "No hay a quien saludar";
+    break;
+}
